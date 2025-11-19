f2fe438e561389bca1ae50f56d1bba7b02858645
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2025-11-19 13:50:46 +0000

    Lecture 174 - Implementing keyboard events and handlers

--- PeachOS64Bit/src/keyboard/keyboard.c
+++ PeachOS64Bit/src/keyboard/keyboard.c
@@ -2,18 +2,19 @@
 #include "status.h"
 #include "kernel.h"
 #include "task/process.h"
+#include "heap/kheap.h"
 #include "task/task.h"
 #include "classic.h"
 
-static struct keyboard* keyboard_list_head = 0;
-static struct keyboard* keyboard_list_last = 0;
+static struct keyboard *keyboard_list_head = 0;
+static struct keyboard *keyboard_list_last = 0;
 
 void keyboard_init()
 {
     keyboard_insert(classic_init());
 }
 
-int keyboard_insert(struct keyboard* keyboard)
+int keyboard_insert(struct keyboard *keyboard)
 {
     int res = 0;
     if (keyboard->init == 0)
@@ -26,50 +27,58 @@ int keyboard_insert(struct keyboard* keyboard)
     {
         keyboard_list_last->next = keyboard;
         keyboard_list_last = keyboard;
-    
     }
     else
     {
         keyboard_list_head = keyboard;
         keyboard_list_last = keyboard;
     }
-    
+
     res = keyboard->init();
+    if (res >= 0)
+    {
+        if (keyboard->key_listeners != NULL)
+        {
+            panic("Let the keyboard.c file make the key listeners vectors");
+        }
+
+        keyboard->key_listeners = vector_new(sizeof(struct keyboard_listener *), 4, 0);
+    }
 out:
     return res;
 }
 
-static int keyboard_get_tail_index(struct process* process)
+static int keyboard_get_tail_index(struct process *process)
 {
     return process->keyboard.tail % sizeof(process->keyboard.buffer);
 }
 
-void keyboard_backspace(struct process* process)
+void keyboard_backspace(struct process *process)
 {
-    process->keyboard.tail -=1 ;
+    process->keyboard.tail -= 1;
     int real_index = keyboard_get_tail_index(process);
     process->keyboard.buffer[real_index] = 0x00;
 }
 
-void keyboard_set_capslock(struct keyboard* keyboard, KEYBOARD_CAPS_LOCK_STATE state)
+void keyboard_set_capslock(struct keyboard *keyboard, KEYBOARD_CAPS_LOCK_STATE state)
 {
     keyboard->capslock_state = state;
 }
 
-KEYBOARD_CAPS_LOCK_STATE keyboard_get_capslock(struct keyboard* keyboard)
+KEYBOARD_CAPS_LOCK_STATE keyboard_get_capslock(struct keyboard *keyboard)
 {
     return keyboard->capslock_state;
 }
 
 void keyboard_push(char c)
 {
-    struct process* process = process_current();
+    struct process *process = process_current();
     if (!process)
     {
         return;
     }
 
-    if(c == 0)
+    if (c == 0)
     {
         return;
     }
@@ -77,6 +86,12 @@ void keyboard_push(char c)
     int real_index = keyboard_get_tail_index(process);
     process->keyboard.buffer[real_index] = c;
     process->keyboard.tail++;
+
+    struct keyboard_event keyboard_event = {0};
+    keyboard_event.type = KEYBOARD_EVENT_KEY_PRESS;
+    keyboard_event.data.key_press.key = c;
+
+    keyboard_push_event_to_listeners(keyboard_default(), &keyboard_event);
 }
 
 char keyboard_pop()
@@ -86,7 +101,7 @@ char keyboard_pop()
         return 0;
     }
 
-    struct process* process = task_current()->process;
+    struct process *process = task_current()->process;
     int real_index = process->keyboard.head % sizeof(process->keyboard.buffer);
     char c = process->keyboard.buffer[real_index];
     if (c == 0x00)
@@ -98,4 +113,85 @@ char keyboard_pop()
     process->keyboard.buffer[real_index] = 0;
     process->keyboard.head++;
     return c;
+}
+
+struct keyboard *keyboard_default()
+{
+    return keyboard_list_head;
+}
+
+int keyboard_register_handler(struct keyboard *keyboard, struct keyboard_listener keyboard_listener)
+{
+    int res = 0;
+    struct keyboard_listener *listener_clone = NULL;
+    if (keyboard == NULL)
+    {
+        keyboard = keyboard_default();
+    }
+
+    if (!keyboard)
+    {
+        res = -EINVARG;
+        goto out;
+    }
+
+    listener_clone = kzalloc(sizeof(struct keyboard_listener));
+    if (!listener_clone)
+    {
+        res = -ENOMEM;
+        goto out;
+    }
+
+    memcpy(listener_clone, &keyboard_listener, sizeof(struct keyboard_listener));
+
+    // now we need to push it the vector
+    vector_push(keyboard->key_listeners, &listener_clone);
+
+out:
+    return res;
+}
+
+void keyboard_push_event_to_listeners(struct keyboard* keyboard, struct keyboard_event* event)
+{
+    size_t total_children = vector_count(keyboard->key_listeners);
+    for(size_t i = 0; i < total_children; i++)
+    {
+        struct keyboard_listener* listener = NULL;
+        vector_at(keyboard->key_listeners, i, &listener, sizeof(listener));
+        if (listener)
+        {
+            listener->on_event(keyboard, event);
+        }    
+    }
+}
+int keyboard_unregister_handler(struct keyboard* keyboard, struct keyboard_listener keyboard_listener)
+{
+    int res = 0;
+    struct keyboard_listener* keyboard_listener_ptr = keyboard_get_listener_ptr(keyboard, keyboard_listener);
+    if (keyboard_listener_ptr)
+    {
+        vector_pop_element(keyboard->key_listeners, &keyboard_listener_ptr, sizeof(keyboard_listener_ptr));
+    }
+
+    kfree(keyboard_listener_ptr);
+    return res;
+}
+
+struct keyboard_listener *keyboard_get_listener_ptr(struct keyboard *keyboard, struct keyboard_listener keyboard_listener)
+{
+    struct keyboard_listener *listener_heap_ptr = NULL;
+    size_t total_children = vector_count(keyboard->key_listeners);
+    for (size_t i = 0; i < total_children; i++)
+    {
+        struct keyboard_listener *current_listener = NULL;
+        vector_at(keyboard->key_listeners, i, &current_listener, sizeof(current_listener));
+        if (current_listener &&
+            keyboard_listener.on_event && keyboard_listener.on_event == current_listener->on_event)
+        {
+            listener_heap_ptr = current_listener;
+            break;
+        }
+    }
+
+    return listener_heap_ptr;
 }
--- PeachOS64Bit/src/keyboard/keyboard.h
+++ PeachOS64Bit/src/keyboard/keyboard.h
@@ -1,10 +1,44 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include "lib/vector/vector.h"
 #define KEYBOARD_CAPS_LOCK_ON 1
 #define KEYBOARD_CAPS_LOCK_OFF 0
 
 typedef int KEYBOARD_CAPS_LOCK_STATE;
+
+typedef int KEYBOARD_EVENT_TYPE;
+enum
+{
+    KEYBOARD_EVENT_INVALID,
+    KEYBOARD_EVENT_KEY_PRESS,
+    KEYBOARD_EVENT_CAPS_LOCK_CHANGE,
+};
+
+struct keyboard_event
+{
+    KEYBOARD_EVENT_TYPE type;
+    union 
+    {
+        struct
+        {
+            int key;
+        } key_press;
+
+        struct
+        {
+            KEYBOARD_CAPS_LOCK_STATE state;
+        } caps_lock;
+    } data;
+    struct keyboard* keyboard;
+};
+
+typedef void (*KEYBOARD_EVENT_LISTENER_ON_EVENT)(struct keyboard* keyboard, struct keyboard_event* event);
+
+struct keyboard_listener
+{
+    KEYBOARD_EVENT_LISTENER_ON_EVENT on_event;
+};
 
 struct process;
 
@@ -16,6 +50,8 @@ struct keyboard
 
     KEYBOARD_CAPS_LOCK_STATE capslock_state;
 
+    // vector of struct keyboard_listener*
+    struct vector* key_listeners;
     struct keyboard* next;
 };
 
@@ -26,5 +62,10 @@ char keyboard_pop();
 int keyboard_insert(struct keyboard* keyboard);
 void keyboard_set_capslock(struct keyboard* keyboard, KEYBOARD_CAPS_LOCK_STATE state);
 KEYBOARD_CAPS_LOCK_STATE keyboard_get_capslock(struct keyboard* keyboard);
+int keyboard_register_handler(struct keyboard* keyboard, struct keyboard_listener keyboard_listener);
+struct keyboard* keyboard_default();
 
+struct keyboard_listener *keyboard_get_listener_ptr(struct keyboard *keyboard, struct keyboard_listener keyboard_listener);
+int keyboard_unregister_handler(struct keyboard* keyboard, struct keyboard_listener keyboard_listener);
+void keyboard_push_event_to_listeners(struct keyboard* keyboard, struct keyboard_event* event);
 #endif
