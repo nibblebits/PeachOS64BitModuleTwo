e98729567ce0b1013a2f5cdc87a97b4223c81349
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2026-02-20 11:43:28 +0000

    added licensing

--- PeachOS64Bit/src/idt/idt.h
+++ PeachOS64Bit/src/idt/idt.h
@@ -1,3 +1,30 @@
+/*
+ * PeachOS 64-Bit Kernel Project
+ * Copyright (C) 2026 Daniel McCarthy <daniel@dragonzap.com>
+ *
+ * This file is part of the PeachOS 64-Bit Kernel.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ * For full source code, documentation, and structured learning,
+ * see the official kernel development course part one:
+ * https://dragonzap.com/course/developing-a-multithreaded-kernel-from-scratch
+ *
+ * Get part one and part two module one, module two all peachos courses (69 hours of content): https://dragonzap.com/offer/kernel-development-from-scratch-69-hours
+ *
+ * Get the part two course module one and two: https://dragonzap.com/offer/developing-a-multithreaded-kernel-from-scratch-part-two-full-series
+ */
+
 #ifndef IDT_H
 #define IDT_H
 
